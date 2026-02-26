206b483ce22e4425e5da1e382991395fe9e1de7c
yagovysk <yago09regis@gmail.com>
2026-02-25 21:15:45 -0300

    Add explicit Google verification head fallback

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="L6Y6Sby_XlHEUMToZYAcwNCLejk51LXX2xzu-3t0Y38"
+      />
+    </>
+  );
+}
